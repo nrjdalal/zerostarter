d29254cccc615876784f97f807d01d1b26dfcf10
Neeraj Dalal <admin@nrjdalal.com>
2025-11-26 01:45:58 +0530

    chore: add hono

--- web/next/next.config.ts
+++ web/next/next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next"
 
+const API_URL = process.env.API_URL || "http://localhost:4000"
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${API_URL}/:path*`,
+      },
+    ]
+  },
 }
 
 export default nextConfig
