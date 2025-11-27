48f1b0f79e5a68ed3ac522996409365544debd86
Neeraj Dalal <admin@nrjdalal.com>
2025-11-28 00:32:19 +0530

    chore: update env

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -13,15 +13,15 @@ if (typeof window === "undefined") {
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "production"]).default("development"),
-    BETTER_AUTH_SECRET: z.string().min(1),
-    GITHUB_CLIENT_ID: z.string().min(1),
-    GITHUB_CLIENT_SECRET: z.string().min(1),
+    BETTER_AUTH_SECRET: z.string().default("openssl rand -base64 32"),
+    GITHUB_CLIENT_ID: z.string().default("https://github.com/settings/developers"),
+    GITHUB_CLIENT_SECRET: z.string().default("https://github.com/settings/developers"),
     HONO_PUBLIC_APP_URL: z.url().default("http://localhost:4000"),
     HONO_PUBLIC_ORIGINS: z
       .string()
       .default("http://localhost:3000")
       .transform((s) => s.split(",")),
-    POSTGRES_URL: z.url(),
+    POSTGRES_URL: z.url().default("postgresql://postgres:postgres@localhost:5432/postgres"),
   },
   clientPrefix: "NEXT_PUBLIC_",
   client: {
