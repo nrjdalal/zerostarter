f973de08a3f53bda22a580edf79c273f5d7e0de9
Neeraj Dalal <admin@nrjdalal.com>
2025-11-28 04:46:22 +0530

    test: ci skip

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -13,15 +13,23 @@ if (typeof window === "undefined") {
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "production"]).default("development"),
-    BETTER_AUTH_SECRET: z.string().default("openssl rand -base64 32"),
-    GITHUB_CLIENT_ID: z.string().default("https://github.com/settings/developers"),
-    GITHUB_CLIENT_SECRET: z.string().default("https://github.com/settings/developers"),
+    BETTER_AUTH_SECRET: !!process.env.CI
+      ? z.string().default("openssl rand -base64 32")
+      : z.string().min(1),
+    GITHUB_CLIENT_ID: !!process.env.CI
+      ? z.string().default("https://github.com/settings/developers")
+      : z.string().min(1),
+    GITHUB_CLIENT_SECRET: !!process.env.CI
+      ? z.string().default("https://github.com/settings/developers")
+      : z.string().min(1),
     HONO_PUBLIC_APP_URL: z.url().default("http://localhost:4000"),
     HONO_PUBLIC_ORIGINS: z
       .string()
       .default("http://localhost:3000")
       .transform((s) => s.split(",")),
-    POSTGRES_URL: z.url().default("postgresql://postgres:postgres@localhost:5432/postgres"),
+    POSTGRES_URL: !!process.env.CI
+      ? z.url().default("postgresql://postgres:postgres@localhost:5432/postgres")
+      : z.url(),
   },
   clientPrefix: "NEXT_PUBLIC_",
   client: {
