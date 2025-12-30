562bd9c724771557fe27d886bf973a475056e2c8
Neeraj Dalal <admin@nrjdalal.com>
2025-12-30 20:01:57 +0530

    chore: not found and error logging at hono

--- api/hono/src/index.ts
+++ api/hono/src/index.ts
@@ -6,6 +6,7 @@ import { authRouter, v1Router } from "@/routers"
 import type { Session } from "@packages/auth"
 import { isLocal } from "@packages/env"
 import { env } from "@packages/env/api-hono"
+import { z } from "zod"
 
 const app = new Hono<{ Variables: Session }>().basePath("/api")
 
@@ -29,6 +30,15 @@ const routes = app
   })
   .route("/auth", authRouter)
   .route("/v1", v1Router)
+  .notFound((c) => c.json({ message: "Not Found" }, 404))
+  .onError((error, c) => {
+    console.error(error)
+    if (error instanceof z.ZodError) {
+      return c.json({ error: { message: "ZodError", errors: JSON.parse(error.message) } }, 400)
+    }
+    if (error instanceof Error) return c.json({ error: { message: error.message } }, 500)
+    return c.json({ error: { message: "I'm a teapot" } }, 418)
+  })
 
 export type AppType = typeof routes
 
