14827a9dad21f53e986e9e8e39c24a9fb06b7142
Neeraj Dalal <admin@nrjdalal.com>
2026-01-29 19:45:31 +0530

    chore: add to fe

--- web/next/src/lib/auth/client.ts
+++ web/next/src/lib/auth/client.ts
@@ -1,11 +1,11 @@
-import { magicLinkClient } from "better-auth/client/plugins"
+import { magicLinkClient, organizationClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 
 import { config } from "@/lib/config"
 
 export const authClient = createAuthClient({
   baseURL: `${config.api.url}/api/auth`,
-  plugins: [magicLinkClient()],
+  plugins: [magicLinkClient(), organizationClient()],
 })
 
 export const { useSession, signIn, signUp, signOut, resetPassword } = authClient
