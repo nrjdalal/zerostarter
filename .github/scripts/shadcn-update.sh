99cd21c47a41a544ec2321a4d7d30f525e8edb17
Neeraj Dalal <admin@nrjdalal.com>
2026-03-14 20:22:53 +0530

    chore: shadcn sync

--- .github/scripts/shadcn-update.sh
+++ .github/scripts/shadcn-update.sh
@@ -2,10 +2,7 @@
 
 cd web/next || exit 1
 rm -rf components.json src/components/ui
-bunx shadcn@latest init . --template next --preset "https://ui.shadcn.com/init?base=base&style=nova&baseColor=neutral&theme=neutral&iconLibrary=remixicon&font=inter&menuAccent=subtle&menuColor=default&radius=default"
+bunx shadcn@latest init --template next --preset a16C8 --base base
 bunx shadcn@latest add -a
-rm -rf src/components/component-example.tsx src/components/example.tsx
 cd ../..
 bun run format
-bun i
-git restore web/next/src/app/layout.tsx
