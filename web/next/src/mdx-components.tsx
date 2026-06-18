45ad0a9ff2a587b5e0f82ae1a6a81875a97a3efa
Neeraj Dalal <admin@nrjdalal.com>
2026-06-18 12:26:15 +0530

    refactor(web): group blog post list component

--- web/next/src/mdx-components.tsx
+++ web/next/src/mdx-components.tsx
@@ -1,7 +1,7 @@
 import defaultMdxComponents from "fumadocs-ui/mdx"
 import type { MDXComponents } from "mdx/types"
 
-import { BlogPostList } from "@/components/blog-post-list"
+import { BlogPostList } from "@/components/blog/post-list"
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
