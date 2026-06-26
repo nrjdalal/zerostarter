89a139aab8f39710b26b8505162c94ccfdb9dee0
Neeraj Dalal <admin@nrjdalal.com>
2026-06-26 11:29:37 +0530

    chore: enable pagination

--- web/next/src/components/data-table/data-table.tsx
+++ web/next/src/components/data-table/data-table.tsx
@@ -1,6 +1,12 @@
 "use client"
 
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+} from "@tanstack/react-table"
 
 import {
   Table,
@@ -21,6 +27,7 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   })
 
   return (
